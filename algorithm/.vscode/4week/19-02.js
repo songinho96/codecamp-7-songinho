c14288d67d83fb7fc01b1def0e4ce6262d6daf14
songinho96 <inhosong96@gmail.com>
2022-06-03 21:40:28 +0900

    알고리즘

---
+++ algorithm/.vscode/4week/19-02.js
@@ -0,0 +1,29 @@
+// 문제 설명
+// 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+// 제한 조건
+// arr은 길이 1 이상인 배열입니다.
+// 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+// 입출력 예
+// arr	return
+// [4,3,2,1]	[4,3,2]
+// [10]	[-1]
+
+function solution(arr) {
+  const answer = [];
+
+  // 1. 제일 작은 수 찾기
+  let min = arr[0]; // 현재 작은 값을 저장하는 변수
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  // 2. 제일 작은 수를 배열에서 제거
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== min) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer.length === 0 ? [-1] : answer;
+}
