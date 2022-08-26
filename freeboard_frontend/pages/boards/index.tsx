3b5afc7dae3745c904a7825530fb046b63ff842b
songinho96 <inhosong96@gmail.com>
2022-08-26 23:55:44 +0900

    :커밋:

--- freeboard_frontend/pages/boards/index.tsx
+++ freeboard_frontend/pages/boards/index.tsx
@@ -1,9 +1,7 @@
 import BestBoardsContainer from "../../src/components/commons/BestBoards/basic/BestBoards.container";
-import { useAuth } from "../../src/components/commons/hooks/useAuth";
 import BoardList from "../../src/components/units/board/list/BoardList.container";
 
 export default function BoardsPage() {
-  useAuth();
   return (
     <>
       <BestBoardsContainer />
